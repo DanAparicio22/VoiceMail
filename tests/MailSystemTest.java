0d323ec70828e5ecdf6f39d4dd0a71047e01d058
kevin camacho <kevinacho_z900@hotmail.com>
2018-03-30 18:14:50 -0400

    Add MailSystemTest class and code

---
+++ tests/MailSystemTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test; 
+import org.junit.Before; 
+public class MailSystemTest {
+
+	MailSystem mailSystem;
+	
+	@Before
+	public void init() {
+		mailSystem = new MailSystem(10);
+	}
+	
+	@Test
+	public void deberiaDevolverUnMailBoxBuscado() {
+		assertNotNull(mailSystem.findMailbox("1"));
+	}
+	
+	@Test
+	public void deberiaNoDevolverUnMailBoxBuscado() {
+		assertNull(mailSystem.findMailbox("15"));
+	}
+}
